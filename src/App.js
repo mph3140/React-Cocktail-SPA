be8ad92fe6cb3d5d8d9dcb4bb3cb728e15f8ca5c
Matthew Hanna <mph3140@gmail.com>
2019-11-13 16:23:18 -0500

    Pulled code for populating array with response data outside of didComponentMount into its own function. Used promise and .map for cleaner look. Additionally added css for RingLoader to let the user know its loading data

--- src/App.js
+++ src/App.js
@@ -1,69 +1,94 @@
-import React from 'react';
-import Form from "./components/form"
-import Titles from "./components/titles"
-import logo from './logo.svg';
-import './App.css';
-// import axios from 'axios';
+import React from "react";
+import Form from "./components/form";
+import Titles from "./components/titles";
+import logo from "./logo.svg";
+import "./App.css";
+import { css } from "@emotion/core";
+import RingLoader from "react-spinners/RingLoader";
 
-const axios = require('axios')
+const axios = require("axios");
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
 
 class App extends React.Component {
-  
-
   constructor() {
-    super()
+    super();
+
     this.state = {
+      loading: true,
       drinks: []
-    }
+    };
   }
 
-  componentDidMount = () => {
+  axCall() {
+    const options = {
+      method: "GET",
+      headers: {
+        "content-type": "application/octet-stream",
+        "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+        "x-rapidapi-key": "4769e24f8fmshe32fdfc0bcb4eb9p180b95jsnf3e30198cfff"
+      },
+      params: {
+        i: "list"
+      }
+    };
 
-      const options = {
-        "method":"GET",
-        "headers":{
-        "content-type":"application/octet-stream",
-        "x-rapidapi-host":"the-cocktail-db.p.rapidapi.com",
-        "x-rapidapi-key":"4769e24f8fmshe32fdfc0bcb4eb9p180b95jsnf3e30198cfff"
-        },"params":{
-        "i":"list"
-        }    
-      };
-  
-      return axios.get('https://the-cocktail-db.p.rapidapi.com/list.php', options)
-        .then((response)=>{
-          console.log(response)
-          var tmpArray = []
-          for (var i = 0; i < response.data.drinks.length; i++){
-            tmpArray.push(<li>{response.data.drinks[i].strIngredient1}</li>)
+    var drinksTemp = [];
+
+    return new Promise((resolve, reject) => {
+      axios
+        .get("https://the-cocktail-db.p.rapidapi.com/list.php", options)
+        .then(response => {
+          console.log(response);
+          for (var i = 0; i < response.data.drinks.length; i++) {
+            drinksTemp.push({
+              drinkName: response.data.drinks[i].strIngredient1
+            });
+            resolve(this.setState({ drinks: drinksTemp, loading: false }));
           }
-          this.setState({
-            drinks: tmpArray      //unsure if need for temporary array. Consider removing later
-          })
         })
-        .catch((error) => {
-          console.error(error)
-        })
-    
+        .catch(error => {
+          console.error(error);
+          reject(error);
+        });
+    });
+  }
+
+  componentDidMount() {
+    this.axCall();
   }
 
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h1>Liqours</h1>
+          <h1>Liquors</h1>
         </div>
-        <div classname="list">
-          <ol>{this.state.drinks}</ol>
+        <div>
+          <RingLoader
+            css={override}
+            sizeUnit={"px"}
+            size={150}
+            color={"#00e68a"}
+            loading={this.state.loading}
+          />
+        </div>
+        <div className="list">
+          <ol>
+            {this.state.drinks.map((drink, index) => {
+              return <li key={index}>{drink.drinkName}</li>;
+            })}
+          </ol>
         </div>
       </div>
-    )
+    );
   }
 }
 export default App;
-
-
-
 
 // function App() {
 //   return (
